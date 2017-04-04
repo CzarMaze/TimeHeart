556dd6c4700267f26f8ac2f51270eb8c7e4d80cb
CzarMaze <a7337266@gmail.com>
2017-04-04 11:04:30 +0800

    teamUI的按鈕已改好

---
+++ Assets/Scripts/OnTriggerA.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using UnityEngine.UI;
+
+public class OnTriggerA : MonoBehaviour {
+
+
+}
