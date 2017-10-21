9fad93475682fe28d05dc35daad31eccee383380
Charlotte <jerry0482000@gmail.com>
2017-10-21 13:26:53 +0800

    按鈕測試(戰鬥)

---
+++ Assets/Scripts/battle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class battle : MonoBehaviour {
+	Animator team1,team2,team3;
+	// Use this for initialization
+	void Start () {
+		team1=GameObject.Find("Team1").gameObject.transform.GetChild(0).GetComponent<Animator>();
+		team2=GameObject.Find("Team2").gameObject.transform.GetChild(0).GetComponent<Animator>();
+		team3=GameObject.Find("Team3").gameObject.transform.GetChild(0).GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	public void attrack(){
+		team1.Play(Animator.StringToHash("3Attack"));
+		Debug.Log("按鈕確定");
+	}
+}
